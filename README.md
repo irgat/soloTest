fc1174c5315762ce374c1cd3f49b35bf7f196f1c
hasim karaman <hasimkaraman@gmail.com>
2024-06-13 13:40:44 +0100

    - initial commit

---
+++ README.md
@@ -0,0 +1,31 @@
+SoloTest
+---
+
+PixiJS TypeScript Code Example
+
+### Prerequisites:
+
+- [nvm](https://nodejs.org/en/download/package-manager)
+```
+$ curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.7/install.sh | bash
+$ nvm install 20
+```
+- [yarn](https://classic.yarnpkg.com/lang/en/docs/install)
+```
+$ npm install --global yarn
+```
+
+### How to build:
+
+```
+$ git clone https://github.com/irgat/soloTest.git soloTest
+$ cd soloTest
+$ yarn
+$ yarn build
+```
+
+### Dev mode:
+
+```
+$ yarn start
+```
---
+++ webpack.common.js
@@ -0,0 +1,32 @@
+const path = require('path');
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    entry: {
+        app: './src/App.ts',
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            title: 'Solo Test',
+        }),
+        new webpack.HotModuleReplacementPlugin(),
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.ts(x)?$/,
+                loader: 'ts-loader',
+                exclude: /node_modules/,
+            },
+        ],
+    },
+    resolve: {
+        extensions: ['.tsx', '.ts', '.js'],
+    },
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'dist'),
+        clean: true
+    },
+};
