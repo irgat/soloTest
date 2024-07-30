65278ccd7789d640e3109cd71deb3924794e561a
hasim karaman <hasimkaraman@gmail.com>
2024-07-30 18:57:56 +0100

    - refactoring

--- src/ui/pages/HomePage/HomePage.styles.ts
+++ src/ui/pages/HomePage/HomePage.styles.ts
@@ -1,17 +1,16 @@
+import { BgTextures, UiTextures } from "../../../App.types";
 import { Point, Polygon } from "pixi.js";
-import { UiTextures } from "../../../App.types";
 
 export const HomePageStyles = {
-    background: {
-    },
+    background: BgTextures.homePage,
     creditsCTA: {
         background: UiTextures.creditsButton_homePage,
         backgroundHover: UiTextures.creditsButton_homePage_hover,
         hitArea: new Polygon([0, 100, 0, 210, 210, 0, 100, 0]),
     },
     playCTA: {
-        background: UiTextures.creditsButton_homePage,
-        backgroundHover: UiTextures.creditsButton_homePage_hover,
+        background: UiTextures.playButton_homePage,
+        backgroundHover: UiTextures.playButton_homePage_hover,
         position: new Point(442, 452),
     },
 }
