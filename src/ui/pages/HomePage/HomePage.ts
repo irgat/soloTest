315adc0ab36bd6265b9f8d09866aac3f86352b1b
hasim karaman <hasimkaraman@gmail.com>
2024-07-30 15:36:09 +0100

    - base button integration

--- src/ui/pages/HomePage/HomePage.ts
+++ src/ui/pages/HomePage/HomePage.ts
@@ -1,4 +1,5 @@
 import { Assets, Sprite } from 'pixi.js';
+import { BaseButton } from '../../components/button/BaseButton';
 import { BasePage } from '../BasePage';
 import { BgTextures, Bundles, GameAssets, UiTextures } from '../../../App.types';
 import { Events } from '../../../common/Events';
@@ -6,11 +7,12 @@ import { HomePageStyles } from './HomePage.styles';
 import { PageIds } from '../Page.types';
 
 /**
- * 
+ * The main page of the app that has two CTAs;
+ * [PLAY] starts the game and [CREDITS] navigates to the credits page.
  */
 export class HomePage extends BasePage {
     private background: Sprite;
-    private creditsCTA: Sprite;
+    private creditsCTA: BaseButton;
 
     public constructor() {
         console.log('HomePage().constructor()');
@@ -44,18 +46,23 @@ export class HomePage extends BasePage {
             console.log('Failed to load BG and/or UI texture/s');
         }
 
-        // add background
+        // create background
         this.background = new Sprite(bgTextures[BgTextures.homePage]);
         this.addChild(this.background);
 
-        // add credits CTA
-        this.creditsCTA = new Sprite(uiTextures[UiTextures.creditsButton_homePage]);
-        this.creditsCTA.anchor = HomePageStyles.creditsCTA.anchor;
+        // create credits CTA
+        const creditsCTA_hitArea = HomePageStyles.creditsCTA.hitArea;
+        const creditsCTA_setup = {
+            default: uiTextures[UiTextures.creditsButton_homePage],
+            hover: uiTextures[UiTextures.creditsButton_homePage_hover],
+        }
+
+        this.creditsCTA = new BaseButton(creditsCTA_setup, creditsCTA_hitArea);
         this.creditsCTA.on(Events.MOUSE_CLICK, this.onCreditsCTASelected);
         this.addChild(this.creditsCTA);
     }
 
     private onCreditsCTASelected = () => {
-        this.emit(Events.SELECTED, PageIds.CreditsPage)
+        this.emit(Events.SELECTED, PageIds.CreditsPage);
     }
 }
