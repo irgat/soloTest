084b1a18a23b1928de5ea512372d0a0e6630edce
hasim karaman <hasimkaraman@gmail.com>
2024-08-19 14:31:33 +0100

    - addressed the change requests

--- src/ui/pages/Page.ts
+++ src/ui/pages/Page.ts
@@ -2,9 +2,19 @@ import { Container } from "pixi.js";
 import { PageId } from "./Page.types";
 
 export class Page extends Container {
-    protected pageId: PageId;
+    private _pageId: PageId;
 
     public constructor(pageId: PageId) {
         super();
+
+        this.pageId = pageId;
+    }
+
+    public get pageId(): PageId {
+        return this._pageId;
+    }
+
+    public set pageId(value: PageId) {
+        this._pageId = value;
     }
 }
